d5597a1a897d334c96d2a0d34bbb4bcb01a64181
Neil Ballard <npballard11@gmail.com>
2024-05-11 16:54:34 +0100

    update docusauraus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/ed_logo.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://edgepulse.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/paulball2/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -25,7 +25,6 @@ const config = {
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
-  
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
